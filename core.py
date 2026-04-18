0424753bb8672c71cbf043dfdce367907fb0063b
KtH15 <hemafarh22@gmail.com>
2026-04-18 04:12:27 +0200

    Refactor random
    Refactor random_lang function to generate random characters based on user input.

--- core.py
+++ core.py
@@ -1,13 +1,9 @@
-#استدعاء مكتبة random
 import random
-#تعين كلمات الOutput
 chars = 'ABCDEFGHIJKLMONPQRSTUVWXYZabcdefghijklmnobqrstuvwxyz'
-#انشاء الداله المسؤلة عن اللغة
 def random_lang ():
     code_input = str(input("~$ "))
     result = ""
     for _ in code_input:
         result += random.choice(chars)
     print(result)
-#تشغيل الداله
 random_lang()
